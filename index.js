5a383edd877dbd300f6dc61cfdae56bdad11d135
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-19 23:43:09 -0500

    created pseudocode based on reade.md

---
+++ index.js
@@ -0,0 +1,32 @@
+const inquirer = require("inquirer");
+
+//prompt take information of team members
+// need the following to be answered on prompt: team manager’s name, employee ID, email address, and office number
+
+// after entering info then list to add to team. options: engineer, intern, finish building team.
+
+//Engineer Prompt: engineer’s name, ID, email, and GitHub username
+
+//prompt done return to main menu
+
+//Intern Prompt: intern’s name, ID, email, and school
+
+//prompt done return to main menu
+
+//when finished exits application
+
+
+//generate HTML file 
+
+
+//formats input of roster
+
+//click email in HTML opens email program
+
+
+//populates to field with email
+
+
+//click Github Username, profile opens in tab
+    //need to create standard link convention
+
